44ed0cefc479d08ec6aec9f0e98e39f9629ce0a8
ArnieTW <arnyziukas@gmail.com>
2026-06-10 21:50:52 +0900

    Add Kick event request models

--- Requests/EventsRequests.cs
+++ Requests/EventsRequests.cs
@@ -4,6 +4,56 @@ using System.Text.Json.Serialization;
 namespace StreamerBot.Requests
 {
     // -------------------------
+    // GetActiveViewers
+    // -------------------------
+    public class GetActiveViewersRequest : StreamerBotRequest
+    {
+        public override string Request => "GetActiveViewers";
+    }
+
+    public class GetActiveViewersResponse : StreamerBotResponse
+    {
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+
+        [JsonPropertyName("viewers")]
+        public ActiveViewer[] Viewers { get; set; } = [];
+    }
+
+    public class ActiveViewer
+    {
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        [JsonPropertyName("type")]
+        public string? Type { get; set; }
+
+        [JsonPropertyName("login")]
+        public string? Login { get; set; }
+
+        [JsonPropertyName("display")]
+        public string? Display { get; set; }
+
+        [JsonPropertyName("subscribed")]
+        public bool Subscribed { get; set; }
+
+        [JsonPropertyName("role")]
+        public string? Role { get; set; }
+
+        [JsonPropertyName("groups")]
+        public string[] Groups { get; set; } = [];
+
+        [JsonPropertyName("channelPointsUsed")]
+        public int ChannelPointsUsed { get; set; }
+
+        [JsonPropertyName("previousActive")]
+        public string? PreviousActive { get; set; }
+
+        [JsonPropertyName("exempt")]
+        public bool Exempt { get; set; }
+    }
+
+    // -------------------------
     // GetEvents
     // -------------------------
     public class GetEventsRequest : StreamerBotRequest
